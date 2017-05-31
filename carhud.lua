e4d94d7477ad7b421b0402e2cadb3699cedbf640
AtomicRSS <rpsthom@gmail.com>
2017-05-31 22:56:01 +0200

    Create carhud.lua

---
+++ carhud.lua
@@ -0,0 +1,36 @@
+function drawTxt(x,y ,width,height,scale, text, r,g,b,a)
+    SetTextFont(4)
+    SetTextProportional(0)
+    SetTextScale(scale, scale)
+    SetTextColour(r, g, b, a)
+    SetTextDropShadow(0, 0, 0, 0,255)
+    SetTextEdge(2, 0, 0, 0, 255)
+    SetTextDropShadow()
+    SetTextOutline()
+    SetTextEntry("STRING")
+    AddTextComponentString(text)
+    DrawText(x - width/2, y - height/2 + 0.005)
+end
+
+function drawRct(x,y,width,height,r,g,b,a)
+	DrawRect(x + width/2, y + height/2, width, height, r, g, b, a)
+end
+
+Citizen.CreateThread(function()
+	while true do
+	
+		Citizen.Wait(1)
+		local get_ped = GetPlayerPed(-1) -- current ped
+		local get_ped_veh = GetVehiclePedIsIn(GetPlayerPed(-1),false) -- Current Vehicle ped is in
+		local pos = GetEntityCoords(GetPlayerPed(-1))
+
+		if(IsPedInAnyVehicle(GetPlayerPed(-1), false))then
+			local kmh = GetEntitySpeed(GetVehiclePedIsIn(GetPlayerPed(-1), false)) * 3.6
+			drawRct(0.157, 0.932, 0.046,0.03,0,0,0,100) 	-- UI:panel kmh	 x + -> y - +
+
+			drawTxt(0.659, 1.42, 1.0,1.0,0.64 , "~w~" .. math.ceil(kmh), 255, 255, 255, 255)  -- INT: kmh
+			drawTxt(0.679, 1.432, 1.0,1.0,0.4, "~w~ km/h", 255, 255, 255, 255)	-- TXT: kmh
+		end		
+		drawTxt(0.51, 0.5155, 1.0,1.0,0.55, "~w~" .. pos,255,255,255,255)
+	end
+end)
